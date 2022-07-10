c00a7e38eed4b73bebf14e73ac9bfa328d15b7f8
robonexx <robertwagar@gmail.com>
2022-07-10 18:10:15 +0200

    Preparation for the next step

--- src/components/ProjectMenu.js
+++ src/components/ProjectMenu.js
@@ -13,7 +13,7 @@ export default function ProjectMenu() {
             {content.map(({ url, title, img }) => (
               <li key={url}>
                 <a href={url}>{title}</a>
-                <img src={img} alt={title}/>
+                <img src={img} alt={title} />
               </li>
             ))}
           </ul>
@@ -45,3 +45,18 @@ const content = [
     img: '/images/bev.png',
   },
 ];
+
+// use this in scss
+// and then intersection observer to
+// create a class so the menu will
+// have and animation to enter screen
+
+/* 
+        @for $i from 0 through 4 {
+            li:nth-child(#{$i + 1}n) {
+                a {
+                    transition-delay: #{$i/10 + 0.8}s;
+                }
+            }
+        }
+ */
